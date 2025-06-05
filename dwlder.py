739a7369b4dba30294e93c1ddd471f103db6330e
dinosaurioinvisible <ferodriguezv@gmail.com>
2025-06-05 18:41:27 -0400

    same

--- dwlder.py
+++ dwlder.py
@@ -4,6 +4,12 @@ import requests
 from bs4 import BeautifulSoup as bs
 import os
 
+# 2020 - 2025  (up to 20/5/25)
+# https://uk.finance.yahoo.com/quote/SQM-B.SN/history/?period1=1577836800&period2=1747785600    
+# ticker = 'SQM-B.SN'
+# start = 15778
+# end = 17476
+
 # specify dir
 os.chdir('../stock_data')
 if os.path.isfile('divs.pkl'):
@@ -12,11 +18,46 @@ else:
     dividends = pd.DataFrame(columns=['Company','Date','Dividend'])
 
 # to get text from a website
-def mk_request(url,headers=0):
+def mk_stock_request(url,headers=0):
     if headers==0:
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
     response = requests.get(url, headers=headers)
     return response.text
+
+# parse stock data from text
+def add_stock_data(text):
+    soup = bs(text, 'html.parser')
+    data = pd.DataFrame(columns=['Date', 'Open','High','Low','Close','AdjClose','Volume'])
+    for day in soup.find('tbody').find_all('tr'):                       # for each day
+        daydata = day.find_all('td')                                    # stock data
+        if len(daydata) > 2:
+            data.loc[len(data)] = [x.text for x in daydata]
+    return data
+
+# parse dividends data from text
+def add_div_data(text,ticker):
+    soup = bs(text, 'html.parser')
+    div_data = pd.DataFrame(columns=['Date',])
+    for day in soup.find('tbody').find_all('tr'):
+        daydata = day.find_all('td')
+        if len(daydata) == 2:                                           # dividends are different (date,div)
+            div_data.loc[len(dividends)] = [ticker]+[x.text.split()[0] for x in daydata]
+    return div_data
+
+# name of ipsa corps
+def get_ipsa_stocks(url_ipsa='https://uk.finance.yahoo.com/quote/%5EIPSA/components/',
+                    start=15778,
+                    end=17476):
+    ipsa_dict = {}
+    text = mk_stock_request(url_ipsa)
+    soup = bs(text, 'html.parser')
+    for tr in soup.find('tbody').find_all('tr'):
+        ticker = tr.find('a').text
+        name = tr.find_all('span')[1].text
+        url = f'https://uk.finance.yahoo.com/quote/{ticker}/history/?period1={start}36800&period2={end}99200'
+        ipsa_dict[ticker] = [ticker, name, url]
+    return ipsa_dict
+
 
 class Stock():
     def __init__(self,ticker,name=None,url=None,data=None,start=15778,end=17476):
@@ -28,9 +69,9 @@ class Stock():
     def get_data(self,start=0,end=0):
         start = start if start > 0 else self.start
         end = end if end > abs(start) else self.end
-        text = mk_request(self.url)
-        soup = bs(text, 'html.parser')
-        self.data = mk_data(soup)
+        text = mk_stock_request(self.url)
+        self.data = add_stock_data(text,self.ticker)
+        self.div_data = add_div_data(text,self.ticker)
     
     def to_pkl(self):
         self.data.to_pickle(f'data_{self.ticker}_{start}_{end}.pkl')
@@ -39,35 +80,13 @@ class Stock():
         self.data.to_csv(f'data_{self.ticker}_{start}_{end}.csv')
         self.dividends.to_csv(f'divs_{start}_{end}.csv')
     
-           
-    
 
 # the idea is to compare EFTs
-class ETF(Stock):
-    def __init__(self,ticker,name=None):
-        super().__init__(ticker,name)
-        self.comps = []
+# class ETF(Stock):
+#     def __init__(self,ticker,name=None):
+#         super().__init__(ticker,name)
+#         self.comps = []
 
-
-# name of ipsa corps
-def get_ipsa_stocks(url_ipsa='https://uk.finance.yahoo.com/quote/%5EIPSA/components/',
-                    start=15778,
-                    end=17476):
-    ipsa_dict = {}
-    text = mk_request(url_ipsa)
-    soup = bs(text, 'html.parser')
-    for tr in soup.find('tbody').find_all('tr'):
-        ticker = tr.find('a').text
-        name = tr.find_all('span')[1].text
-        url = f'https://uk.finance.yahoo.com/quote/{ticker}/history/?period1={start}36800&period2={end}99200'
-        ipsa_dict[ticker] = [ticker, name, url]
-    return ipsa_dict
-
-# 2020 - 2025  (up to 20/5/25)
-# https://uk.finance.yahoo.com/quote/SQM-B.SN/history/?period1=1577836800&period2=1747785600    
-# ticker = 'SQM-B.SN'
-# start = 15778
-# end = 17476          
 
 
 def get_stocks_data(tickers,start=0,end=0):
@@ -103,7 +122,7 @@ def get_data(ticker,start=15778,end=17476,to_csv=False,to_pkl=False):
     # get data
     # response = requests.get(url, headers=headers)
     # web = response.text
-    text = mk_request(url)
+    text = mk_stock_request(url)
     soup = bs(text, 'html.parser')
     # data to df
     data = pd.DataFrame(columns=['Date', 'Open','High','Low','Close','AdjClose','Volume'])
@@ -114,12 +133,12 @@ def get_data(ticker,start=15778,end=17476,to_csv=False,to_pkl=False):
         else:
             data.loc[len(data)] = [x.text for x in daydata]
     # save
-    if to_csv:
-        data.to_csv(f'data_{ticker}_{start}_{end}.csv')
-        dividends.to_csv(f'divs_{start}_{end}.csv')
-    if to_pkl:
-        data.to_pickle(f'data_{ticker}_{start}_{end}.pkl')
-        dividends.to_pickle(f'divs_{start}_{end}.pkl')
+    # if to_csv:
+    #     data.to_csv(f'data_{ticker}_{start}_{end}.csv')
+    #     dividends.to_csv(f'divs_{start}_{end}.csv')
+    # if to_pkl:
+    #     data.to_pickle(f'data_{ticker}_{start}_{end}.pkl')
+    #     dividends.to_pickle(f'divs_{start}_{end}.pkl')
     return data
 
 if __name__ == '__main__':
